6667869da3a2d2648eba4e40bff6fa424a1d937e
666stephunter <stepiktheslayer>
2018-12-10 08:15:59 +0700

    Done.

---
+++ project7.py
@@ -0,0 +1,315 @@
+male = input('Введите ваш знак зодиака:')
+female = input('Введите знак зодиака вашего партнёра:')
+
+prediction = ['Борьба, споры', 'Недолгие отношения', 'Отличная пара', 'Они созданы друг для друга',
+              'Страстные отношения, счастливая пара', 'Возможно, что все будет хорошо', 'Возможно разочарование',
+              'Отношения непредсказуемы', 'Дружба, радость', 'Оба упрямы, неизвестно, кто возьмет верх',
+              'Необычайные, полные событий отношения', 'Встречи жизнерадостные и счастливые', 'Полное взаимопонимание',
+              'Разные темпераменты', 'Хорошая пара, понимают друг друга',
+              'Оба хотят быть в центре внимания, кому-то надо уступать', 'Не упрямьтесь, и все будет хорошо',
+              'Крепкие отношения, много общего', 'Одна минута – и пожар', 'Нет взаимопонимания',
+              'Все отлично в дружбе и любви', 'Могут наскучить друг другу', 'Долгие и теплые отношения',
+              'Полное взаимопонимание', ' Полное взаимопонимание',' Много обид и недомолвок',' Великолепная пара',
+              'Им будет не очень хорошо вместе', ' Нежные отношения, радость в общении',
+              'Сильное притяжение друг к другу', ' Научитесь слышать друг друга', 'Только сильная любовь их соединит',
+              'Вас ждет много приключений и радости', 'Слишком разные, чтобы быть счастливыми',
+              'Много фантазий и мечтаний', 'Скука в отношениях']
+if male == 'овен' and female == 'овен':
+    print(prediction[0])
+elif male == 'овен' and female == 'телец':
+    print(prediction[1])
+elif male == 'овен' and female == 'близнецы':
+    print(prediction[2])
+elif male == 'овен' and female == 'рак':
+    print(prediction[3])
+elif male == 'овен' and female == 'лев':
+    print(prediction[4])
+elif male == 'овен' and female == 'дева':
+    print(prediction[5])
+elif male == 'овен' and female == 'весы':
+    print(prediction[6])
+elif male == 'овен' and female == 'скорпион':
+    print(prediction[7])
+elif male == 'овен' and female == 'стрелец':
+    print(prediction[8])
+elif male == 'овен' and female == 'козерог':
+    print(prediction[9])
+elif male == 'овен' and female == 'водолей':
+    print(prediction[10])
+elif male == 'овен' and female == 'рыбы':
+    print(prediction[11])
+
+elif male == 'телец' and female == 'овен':
+    print(prediction[12])
+elif male == 'телец' and female == 'телец':
+    print(prediction[13])
+elif male == 'телец' and female == 'близнецы':
+    print(prediction[14])
+elif male == 'телец' and female == 'рак':
+    print(prediction[15])
+elif male == 'телец' and female == 'лев':
+    print(prediction[16])
+elif male == 'телец' and female == 'дева':
+    print(prediction[17])
+elif male == 'телец' and female == 'весы':
+    print(prediction[18])
+elif male == 'телец' and female == 'скорпион':
+    print(prediction[19])
+elif male == 'телец' and female == 'стрелец':
+    print(prediction[20])
+elif male == 'телец' and female == 'козерог':
+    print(prediction[21])
+elif male == 'телец' and female == 'водолей':
+    print(prediction[22])
+elif male == 'телец' and female == 'рыбы':
+    print(prediction[23])
+
+elif male == 'близнецы' and female == 'овен':
+    print(prediction[24])
+elif male == 'близнецы' and female == 'телец':
+    print(prediction[25])
+elif male == 'близнецы' and female == 'близнецы':
+    print(prediction[26])
+elif male == 'близнецы' and female == 'рак':
+    print(prediction[27])
+elif male == 'близнецы' and female == 'лев':
+    print(prediction[28])
+elif male == 'близнецы' and female == 'дева':
+    print(prediction[29])
+elif male == 'близнецы' and female == 'весы':
+    print(prediction[30])
+elif male == 'близнецы' and female == 'скорпион':
+    print(prediction[31])
+elif male == 'близнецы' and female == 'стрелец':
+    print(prediction[32])
+elif male == 'близнецы' and female == 'козерог':
+    print(prediction[33])
+elif male == 'близнецы' and female == 'водолей':
+    print(prediction[34])
+elif male == 'близнецы' and female == 'рыбы':
+    print(prediction[35])
+
+elif male == 'рак' and female == 'овен':
+    print(prediction[0])
+elif male == 'рак' and female == 'телец':
+    print(prediction[1])
+elif male == 'рак' and female == 'близнецы':
+    print(prediction[2])
+elif male == 'рак' and female == 'рак':
+    print(prediction[3])
+elif male == 'рак' and female == 'лев':
+    print(prediction[4])
+elif male == 'рак' and female == 'дева':
+    print(prediction[5])
+elif male == 'рак' and female == 'весы':
+    print(prediction[6])
+elif male == 'рак' and female == 'скорпион':
+    print(prediction[7])
+elif male == 'рак' and female == 'стрелец':
+    print(prediction[8])
+elif male == 'рак' and female == 'козерог':
+    print(prediction[9])
+elif male == 'рак' and female == 'водолей':
+    print(prediction[10])
+elif male == 'рак' and female == 'рыбы':
+    print(prediction[11])
+
+elif male == 'лев' and female == 'овен':
+    print(prediction[12])
+elif male == 'лев' and female == 'телец':
+    print(prediction[13])
+elif male == 'лев' and female == 'близнецы':
+    print(prediction[14])
+elif male == 'лев' and female == 'рак':
+    print(prediction[15])
+elif male == 'лев' and female == 'лев':
+    print(prediction[16])
+elif male == 'лев' and female == 'дева':
+    print(prediction[17])
+elif male == 'лев' and female == 'весы':
+    print(prediction[18])
+elif male == 'лев' and female == 'скорпион':
+    print(prediction[19])
+elif male == 'лев' and female == 'стрелец':
+    print(prediction[20])
+elif male == 'лев' and female == 'козерог':
+    print(prediction[21])
+elif male == 'лев' and female == 'водолей':
+    print(prediction[22])
+elif male == 'лев' and female == 'рыбы':
+    print(prediction[23])
+
+elif male == 'дева' and female == 'овен':
+    print(prediction[24])
+elif male == 'дева' and female == 'телец':
+    print(prediction[25])
+elif male == 'дева' and female == 'близнецы':
+    print(prediction[26])
+elif male == 'дева' and female == 'рак':
+    print(prediction[27])
+elif male == 'дева' and female == 'лев':
+    print(prediction[28])
+elif male == 'дева' and female == 'дева':
+    print(prediction[29])
+elif male == 'дева' and female == 'весы':
+    print(prediction[30])
+elif male == 'дева' and female == 'скорпион':
+    print(prediction[31])
+elif male == 'дева' and female == 'стрелец':
+    print(prediction[32])
+elif male == 'дева' and female == 'козерог':
+    print(prediction[33])
+elif male == 'дева' and female == 'водолей':
+    print(prediction[34])
+elif male == 'дева' and female == 'рыбы':
+    print(prediction[35])
+
+elif male == 'весы' and female == 'овен':
+    print(prediction[0])
+elif male == 'весы' and female == 'телец':
+    print(prediction[1])
+elif male == 'весы' and female == 'близнецы':
+    print(prediction[2])
+elif male == 'весы' and female == 'рак':
+    print(prediction[3])
+elif male == 'весы' and female == 'лев':
+    print(prediction[4])
+elif male == 'весы' and female == 'дева':
+    print(prediction[5])
+elif male == 'весы' and female == 'весы':
+    print(prediction[6])
+elif male == 'весы' and female == 'скорпион':
+    print(prediction[7])
+elif male == 'весы' and female == 'стрелец':
+    print(prediction[8])
+elif male == 'весы' and female == 'козерог':
+    print(prediction[9])
+elif male == 'весы' and female == 'водолей':
+    print(prediction[10])
+elif male == 'весы' and female == 'рыбы':
+    print(prediction[11])
+
+elif male == 'скорпион' and female == 'овен':
+    print(prediction[12])
+elif male == 'скорпион' and female == 'телец':
+    print(prediction[13])
+elif male == 'скорпион' and female == 'близнецы':
+    print(prediction[14])
+elif male == 'скорпион' and female == 'рак':
+    print(prediction[15])
+elif male == 'скорпион' and female == 'лев':
+    print(prediction[16])
+elif male == 'скорпион' and female == 'дева':
+    print(prediction[17])
+elif male == 'скорпион' and female == 'весы':
+    print(prediction[18])
+elif male == 'скорпион' and female == 'скорпион':
+    print(prediction[19])
+elif male == 'скорпион' and female == 'стрелец':
+    print(prediction[20])
+elif male == 'скорпион' and female == 'козерог':
+    print(prediction[21])
+elif male == 'скорпион' and female == 'водолей':
+    print(prediction[22])
+elif male == 'скорпион' and female == 'рыбы':
+    print(prediction[23])
+
+elif male == 'стрелец' and female == 'овен':
+    print(prediction[24])
+elif male == 'стрелец' and female == 'телец':
+    print(prediction[25])
+elif male == 'стрелец' and female == 'близнецы':
+    print(prediction[26])
+elif male == 'стрелец' and female == 'рак':
+    print(prediction[27])
+elif male == 'стрелец' and female == 'лев':
+    print(prediction[28])
+elif male == 'стрелец' and female == 'дева':
+    print(prediction[29])
+elif male == 'стрелец' and female == 'весы':
+    print(prediction[30])
+elif male == 'стрелец' and female == 'скорпион':
+    print(prediction[31])
+elif male == 'стрелец' and female == 'стрелец':
+    print(prediction[32])
+elif male == 'стрелец' and female == 'козерог':
+    print(prediction[33])
+elif male == 'стрелец' and female == 'водолей':
+    print(prediction[34])
+elif male == 'стрелец' and female == 'рыбы':
+    print(prediction[35])
+
+elif male == 'козерог' and female == 'овен':
+    print(prediction[0])
+elif male == 'козерог' and female == 'телец':
+    print(prediction[1])
+elif male == 'козерог' and female == 'близнецы':
+    print(prediction[2])
+elif male == 'козерог' and female == 'рак':
+    print(prediction[3])
+elif male == 'козерог' and female == 'лев':
+    print(prediction[4])
+elif male == 'козерог' and female == 'дева':
+    print(prediction[5])
+elif male == 'козерог' and female == 'весы':
+    print(prediction[6])
+elif male == 'козерог' and female == 'скорпион':
+    print(prediction[7])
+elif male == 'козерог' and female == 'стрелец':
+    print(prediction[8])
+elif male == 'козерог' and female == 'козерог':
+    print(prediction[9])
+elif male == 'козерог' and female == 'водолей':
+    print(prediction[10])
+elif male == 'козерог' and female == 'рыбы':
+    print(prediction[11])
+
+elif male == 'водолей' and female == 'овен':
+    print(prediction[12])
+elif male == 'водолей' and female == 'телец':
+    print(prediction[13])
+elif male == 'водолей' and female == 'близнецы':
+    print(prediction[14])
+elif male == 'водолей' and female == 'рак':
+    print(prediction[15])
+elif male == 'водолей' and female == 'лев':
+    print(prediction[16])
+elif male == 'водолей' and female == 'дева':
+    print(prediction[17])
+elif male == 'водолей' and female == 'весы':
+    print(prediction[18])
+elif male == 'водолей' and female == 'скорпион':
+    print(prediction[19])
+elif male == 'водолей' and female == 'стрелец':
+    print(prediction[20])
+elif male == 'водолей' and female == 'козерог':
+    print(prediction[21])
+elif male == 'водолей' and female == 'водолей':
+    print(prediction[22])
+elif male == 'водолей' and female == 'рыбы':
+    print(prediction[23])
+
+elif male == 'рыбы' and female == 'овен':
+    print(prediction[24])
+elif male == 'рыбы' and female == 'телец':
+    print(prediction[25])
+elif male == 'рыбы' and female == 'близнецы':
+    print(prediction[26])
+elif male == 'рыбы' and female == 'рак':
+    print(prediction[27])
+elif male == 'рыбы' and female == 'лев':
+    print(prediction[28])
+elif male == 'рыбы' and female == 'дева':
+    print(prediction[29])
+elif male == 'рыбы' and female == 'весы':
+    print(prediction[30])
+elif male == 'рыбы' and female == 'скорпион':
+    print(prediction[31])
+elif male == 'рыбы' and female == 'стрелец':
+    print(prediction[32])
+elif male == 'рыбы' and female == 'козерог':
+    print(prediction[33])
+elif male == 'рыбы' and female == 'водолей':
+    print(prediction[34])
+elif male == 'рыбы' and female == 'рыбы':
+    print(prediction[35])
